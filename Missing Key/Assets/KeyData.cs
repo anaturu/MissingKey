31046932824d060acf9636ba471730cd438a6cb8
anaturu <a.marotine@rubika-edu.com>
2024-04-05 19:02:06 +0200

    commentaires + on key up

--- Missing Key/Assets/KeyData.cs
+++ Missing Key/Assets/KeyData.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 
 public class KeyData : MonoBehaviour
 {
+    [Header("DO NOT TOUCH")]
     public string code;
-    public KeyStatus keyStatus;
-    public KeyData[] adjacentKeyDatas;
     
-    public enum KeyStatus
+    public KeyStatus keyStatus; //Variable contenant l'enum "KeyStatus"
+    public KeyData[] adjacentKeyDatas; //Contient  toute les touches adjacentes à la touche
+    public Vector3 keyPos;
+    
+    public enum KeyStatus //Tous les states spéciaux des touches spéciales
     {
-        Basic, Mine, Hole
+        Basic, 
+        Mine, 
+        Hole
     }
 
     private void Start()
     {
         code = gameObject.name;
+        keyPos = transform.position;
     }
 
     public void Explode()
--- Missing Key/Assets/KeyManager.cs
+++ Missing Key/Assets/KeyManager.cs
@@ -4,20 +4,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
+using DG.Tweening;
 
 public class KeyManager : MonoBehaviour
 {
-    [SerializeField] private KeyData[] _keyDatas;
-    
-    private readonly Array keyCodes = Enum.GetValues(typeof(KeyCode));
+    [Header("DO NOT TOUCH")]
+    [SerializeField] private KeyData[] _keyDatas; //Array du script "KeyData"
+    private readonly Array keyCodes = Enum.GetValues(typeof(KeyCode)); //Array contenant TOUTES les touches du clavier
+
+    [SerializeField] private float travelSpeed;
 
     public void Update()
     {
-        if (Input.GetKeyDown(FindLastPressedInput()))
+        if (Input.GetKeyDown(FindLastPressedInput())) //Si je press n'importe quel touche du clavier
         {
             KeyData currentKey = ReturnKeyDataFromInput(FindLastPressedInput());
+
+            #region AllKeys
             
-            switch(currentKey.keyStatus)
+            //DO CODE pour n'importe quelle touche
+            for (int i = 0; i < currentKey.adjacentKeyDatas.Length; i++) // i = int qui représente chaque touche du clavier adjacente à la currentKey une par une 
+            {
+                currentKey.adjacentKeyDatas[i].transform.DOMove(currentKey.adjacentKeyDatas[i].keyPos + new Vector3(0, 1, 0), travelSpeed);
+            }
+            
+            #endregion
+           
+            
+            switch(currentKey.keyStatus) //DO CODE pour les touches spécifiques
+            {
+                case KeyData.KeyStatus.Basic:
+                    
+                    break;
+                case KeyData.KeyStatus.Mine:
+                    currentKey.Explode();
+                    break;
+                case KeyData.KeyStatus.Hole:
+                    
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        if (Input.GetKeyUp(FindLastReleasedInput()))
+        {
+            KeyData currentKey = ReturnKeyDataFromInput(FindLastReleasedInput());
+            
+            Debug.Log("OAJUFOLIAEPIHJAEFP");
+            //DO CODE pour n'importe quelle touche
+            
+            for (int i = 0; i < currentKey.adjacentKeyDatas.Length; i++) // i = int qui représente chaque touche du clavier adjacente à la currentKey une par une 
+            {
+                currentKey.adjacentKeyDatas[i].transform.DOMove(currentKey.adjacentKeyDatas[i].keyPos, travelSpeed);
+            }
+            
+            switch(currentKey.keyStatus) //DO CODE pour les touches spécifiques
             {
                 case KeyData.KeyStatus.Basic:
                     
@@ -34,7 +76,7 @@ public class KeyManager : MonoBehaviour
         }
     }
 
-    KeyData ReturnKeyDataFromInput(KeyCode keyCode)
+    KeyData ReturnKeyDataFromInput(KeyCode keyCode) //Permet de traduire keyCode en KeyData (script)
     {
         KeyData value = new KeyData();
         
@@ -50,12 +92,25 @@ public class KeyManager : MonoBehaviour
         return value;
     }
 
-    KeyCode FindLastPressedInput()
+    KeyCode FindLastPressedInput() //Correspond au dernier Input pressé
     {
         KeyCode value = new KeyCode();
         foreach (KeyCode keyCode in keyCodes)
         {
             if (Input.GetKeyDown(keyCode))
+            {
+                value = keyCode;
+                break;
+            }
+        }
+        return value;
+    }
+    KeyCode FindLastReleasedInput() //Correspond au dernier Input relâché
+    {
+        KeyCode value = new KeyCode();
+        foreach (KeyCode keyCode in keyCodes)
+        {
+            if (Input.GetKeyUp(keyCode))
             {
                 value = keyCode;
                 break;
